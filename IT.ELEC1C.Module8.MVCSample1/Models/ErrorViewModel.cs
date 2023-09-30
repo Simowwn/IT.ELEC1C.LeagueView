6cff16eb796a04d3b4c98368758df8353189f382
karlcedrickmillares <kcm17.developer@gmail.com>
2023-09-30 08:12:32 +0800

    initial commit

---
+++ IT.ELEC1C.Module8.MVCSample1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IT.ELEC1C.Module8.MVCSample1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
